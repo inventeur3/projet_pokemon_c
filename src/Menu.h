9db897fa02c03878280e4e711082e0923b02358c
RomaneABOAB <romaneaboab@gmail.com>
2025-05-06 11:47:37 +0200

    Create Menu.h

---
+++ src/Menu.h
@@ -0,0 +1,11 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "Entraineur.h"
+#include "Pokemon.h"
+
+// Déclaration des fonctions utilisées dans Menu.cpp
+void afficherMenu();
+void Combat(Joueur j, Entraineur e);
+
+#endif // MENU_H
