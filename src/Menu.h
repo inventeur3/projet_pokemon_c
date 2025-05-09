35aa8779ad4bfdbd674e0fd0a163785f8be57b08
RomaneABOAB <romaneaboab@gmail.com>
2025-05-09 10:33:26 +0200

    Update Menu.h

--- src/Menu.h
+++ src/Menu.h
@@ -1,12 +1,9 @@
-#endif // MENU_H
-
 #ifndef MENU_H
 #define MENU_H
 
-#include "Entraineur.h" 
+#include "Entraineur.h"
 #include "Pokemon.h"
 
-// Prototypes des fonctions du menu
 void Combat(Joueur& j, Entraineur& e);
 void afficherMenu();
 
