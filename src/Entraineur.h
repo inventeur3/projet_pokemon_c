fc9c2495bcc91e6621a64677b86492d14c8f8ad3
RomaneABOAB <romaneaboab@gmail.com>
2025-05-09 10:27:51 +0200

    Update Entraineur.h

--- src/Entraineur.h
+++ src/Entraineur.h
@@ -3,23 +3,23 @@
 
 #include <string>
 #include <vector>
+
 using namespace std;
 
-class Pokemon;
-
-
-class Entraineur {
-protected:
-    string nomEntraineur;
-    int PV;
-    Pokemon* actif;
-    Pokemon* pokemons;
-    int indexActuel;
-    int nbPokemons;
+class Entraineur{
+    protected:
+        string nomEntraineur;
+        int PV;
+        Pokemon* actif;
+        Pokemon* pokemons;
+        int indexActuel;
+        int nbPokemons;
+        string message;
+        bool victoire;
 
 public:
     Entraineur(string n1, Pokemon poke1, Pokemon poke2, Pokemon poke3, Pokemon poke4,
-               Pokemon poke5, Pokemon poke6);
+            Pokemon poke5, Pokemon poke6, string mes = "");
     virtual ~Entraineur();
 
     void entrantEnCombat();
@@ -28,59 +28,64 @@ public:
     int getDegat() const;
     string getType1() const;
     string getType2() const;
+    Pokemon* getActif() const;
 
     void attaque(Entraineur& cible);
     void receivedDamage(Entraineur& attaquant);
     void finCombat();
     string getNomEntraineur() const;
+    bool interagir();
 };
 
 class Joueur : public Entraineur {
-private:
-    int nbBadges;
-    int nbCombatsGagnes;
-    int nbCombatsPerdus;
-    void ajouterBadge();
-    void ajouterVictoire();
-    void ajouterDefaite();
+    private:
+        int nbBadges;
+        int nbCombatsGagnes;
+        int nbCombatsPerdus;
+        void ajouterBadge();
 
-public:
-    Joueur(string n1, Pokemon poke1, Pokemon poke2, Pokemon poke3,
-           Pokemon poke4, Pokemon poke5, Pokemon poke6);
 
-    int getBadges() const;
-    int getCombatsGagnes() const;
-    int getCombatsPerdus() const;
+    public:
+        Joueur(string n1, Pokemon poke1, Pokemon poke2, Pokemon poke3,
+            Pokemon poke4, Pokemon poke5, Pokemon poke6);
 
-    void ordre();
-    void demanderMedaille(class Leader& leader);
+        int getBadges() const;
+        int getCombatsGagnes() const;
+        int getCombatsPerdus() const;
+
+        void ajouterVictoire();
+        void ajouterDefaite();
+
+        void ordre();
+        bool interagir(Entraineur& entraineur);
+        void afficher();
+        Pokemon* getPokemon(int i);
 };
 
 class Leader : public Entraineur {
-private:
-    string gymnase;
-    string medaille;
-    int victoire;
-    string badgeAccorde;
+    private:
+        string gymnase;
+        string medaille;
+        bool badgeAccorde;
 
-public:
-    Leader(string n1, Pokemon poke1, Pokemon poke2, Pokemon poke3,
-           Pokemon poke4, Pokemon poke5, Pokemon poke6,
-           string nomGym, string badge, string badgeAccorde);
+    public:
+        Leader(string n1, string nomGym, string badge,
+            Pokemon poke1, Pokemon poke2, Pokemon poke3,
+            Pokemon poke4, Pokemon poke5, Pokemon poke6, string mes = "");
 
-    string getGymnase() const;
-    string getMedaille() const;
-    void accorderMedaille(Joueur& joueur);
+        string getGymnase() const;
+        string getMedaille() const;
+        void accorderMedaille(Joueur& joueur);
+        bool interagir(Joueur& joueur);
 };
 
 class Maitre : public Entraineur {
-public:
-    Maitre(string n1, Pokemon poke1, Pokemon poke2, Pokemon poke3,
-           Pokemon poke4, Pokemon poke5, Pokemon poke6);
+    public:
+        Maitre(string n1, Pokemon poke1, Pokemon poke2, Pokemon poke3,
+            Pokemon poke4, Pokemon poke5, Pokemon poke6, string mes="");
 
-    int getDegatBoosted() const;
-    void affronterMaitre(Joueur& joueur, Maitre& maitre);
+        int getDegatBoosted() const;
+        void affronterMaitre(Joueur& joueur, Maitre& maitre);
 };
 
 #endif // ENTRAINEURGLOBAL_H
-
